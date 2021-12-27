2d01f4afc78bc93e7e8ce7b3b72bea8fee4d7727
Vaibhav Kumar <66525224+vaibhav5219@users.noreply.github.com>
2021-12-27 16:23:09 +0530

    Initial Commit
    Initial commit  Of RDLC

---
+++ RDLCreportDemo/RDLCreportDemo/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace RDLCreportDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.DsSales = new RDLCreportDemo.DsSales();
+            this.UsersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.UsersTableAdapter = new RDLCreportDemo.DsSalesTableAdapters.UsersTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.DsSales)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UsersBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.UsersBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "RDLCreportDemo.SalesReport.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(9, 11);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(747, 704);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // DsSales
+            // 
+            this.DsSales.DataSetName = "DsSales";
+            this.DsSales.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // UsersBindingSource
+            // 
+            this.UsersBindingSource.DataMember = "Users";
+            this.UsersBindingSource.DataSource = this.DsSales;
+            // 
+            // UsersTableAdapter
+            // 
+            this.UsersTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 727);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DsSales)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UsersBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource UsersBindingSource;
+        private DsSales DsSales;
+        private DsSalesTableAdapters.UsersTableAdapter UsersTableAdapter;
+    }
+}
+
